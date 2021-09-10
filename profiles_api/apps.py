77db2bd1ba1c57b674f2a381ba69146b062bb929
Ammar <amaar.alaa93@gmail.com>
2021-09-10 23:07:17 +0300

    create profiles api

--- profiles_api/apps.py
+++ profiles_api/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ApiConfig(AppConfig):
+class ProfilesApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'api'
+    name = 'profiles_api'
--- project/settings.py
+++ project/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'api',
+    'profiles_api',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +126,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL='profiles_api.UserProfile'
